0059499be70c43551812cec9fed10171fa9d8dd5
杨扬（骐轩） <qixuan.yy@alibaba-inc.com>
2015-11-23 11:00:12 +0800

    refined swipeview demo

--- demo/gui/swipeview/myapp.js
+++ demo/gui/swipeview/myapp.js
@@ -3,7 +3,7 @@ var fx = require("framework");
 var Class = fx.import("framework.Class");
 var App = fx.import("framework.app.App");
 var SwipeView = fx.import("framework.ui.view.SwipeView");
-var View = fx.import("framework.ui.view.View");
+var ImageView = fx.import("framework.ui.view.ImageView");
 
 Class.define("MyApp", App, {
     onStart: function() {
@@ -12,36 +12,21 @@ Class.define("MyApp", App, {
         swipeView.width = 320;
         swipeView.height = 320;
 
-        var view1 = new View();
-        view1.background = "#FF0000";
-        swipeView.addChild(view1);
+        var imageView1 = new ImageView();
+        imageView1.src = global.app.rootPath + "/res/porsche1.png";
+        swipeView.addChild(imageView1);
 
-        var view2 = new View();
-        view2.background = "#00FF00";
-        swipeView.addChild(view2);
+        var imageView2 = new ImageView();
+        imageView2.src = global.app.rootPath + "/res/porsche2.png";
+        swipeView.addChild(imageView2);
 
-        var view3 = new View();
-        view3.background = "#0000FF";
-        swipeView.addChild(view3);
+        var imageView3 = new ImageView();
+        imageView3.src = global.app.rootPath + "/res/porsche3.png";
+        swipeView.addChild(imageView3);
 
-        // var view2 = new CompositeView();
-        // view2.background = "#00FF00";
-        // view2.width = 320;
-        // view2.height = 320;
-        // swipeView.addChild(view2);
-
-        // var textView2 = new TextView();
-        // textView2.text = "I am a TextView of CompositeView";
-        // textView2.left = 100;
-        // textView2.width = 320;
-        // textView2.height = 100;
-        // view2.addChild(textView2);
-
-        // var imageView = new ImageView();
-        // imageView.src = global.app.rootPath + "/res/bg_1.png";
-        // imageView.width = 320;
-        // imageView.height = 320;
-        // swipeView.addChild(imageView);
+        var imageView4 = new ImageView();
+        imageView4.src = global.app.rootPath + "/res/porsche4.png";
+        swipeView.addChild(imageView4);
 
         this.window.addChild(swipeView);
     }
