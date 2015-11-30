61fd82d7171d7697d567153671fea30fd83d6ee2
杨扬（骐轩） <qixuan.yy@alibaba-inc.com>
2015-11-30 12:48:22 +0800

    refined docs

--- framework/ui/view/checkbox.js
+++ framework/ui/view/checkbox.js
@@ -14,28 +14,36 @@ var Class = require("../../class");
 var Switch = require("./switch");
 
 /**
- * CheckBox widget
+ * CheckBox is a specific type of two-states button that can be either checked or unchecked.
  * @class CheckBox
  * @extends Switch
  */
 Class.define("framework.ui.view.CheckBox", Switch, {
     /**
-     * Constructor
+     * Constructor that create a checkbox
      * @method CheckBox#initialize
      */
     initialize: function () {
         Switch.prototype.initialize.apply(this, arguments);
-        this._disabledSrc = global.AppFXRootPath + "/resources/checkboxdisabled.png";
-        this._offSrc = global.AppFXRootPath + "/resources/checkboxoff.png";
-        this._onSrc = global.AppFXRootPath + "/resources/checkboxon.png";
     },
 
     /**
-     * Destructor
+     * Destructor that destroy this checkbox
      * @method CheckBox#destroy
      */
     destroy: function() {
         Switch.prototype.destroy.apply(this, arguments);
+    },
+
+    /**
+     * Specifiy the urls of the image state, including on, off and disabled
+     * @method Switch#setImageSrcs
+     * @protected
+     */
+    setImageSrcs: function() {
+        this._disabledImageSrc = global.AppFXRootPath + "/resources/checkboxdisabled.png";
+        this._offImageSrc = global.AppFXRootPath + "/resources/checkboxoff.png";
+        this._onImageSrc = global.AppFXRootPath + "/resources/checkboxon.png";
     }
 }, module);
 
